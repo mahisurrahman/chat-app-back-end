b79f385fa2399c0a1cc355d4f1b7e3810ba17c8d
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-21 23:03:49 +0600

    MongoDb Connect With Mongoose Successfully Completed

--- index.js
+++ index.js
@@ -3,49 +3,56 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cookieParser = require('cookie-parser');
+const { default: mongoose } = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 7000;
 
-//MiddleWares//
-app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173/',
-    methods: ["POST", "GET", "PUT", "PATCH", "UPDATE", "DELETE"],
-    credentials: true,
-}))
+// //MiddleWares//
+// app.use(express.json());
+// app.use(cors({
+//     origin: 'http://localhost:5173/',
+//     methods: ["POST", "GET", "PUT", "PATCH", "UPDATE", "DELETE"],
+//     credentials: true,
+// }))
 
 
-//MongoDB Codes Starts Here//
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b4ql9rm.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+//Mongoose Connect With Mongo DB//
+const connectDb = async()=>{
+  try{
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log('Server Connected to DB');
+  }catch(err){
+    console.log(err.message);
   }
-});
+};
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+connectDb();
 
 
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 
